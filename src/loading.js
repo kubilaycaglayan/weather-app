8e5e8b539b8a04912cabe9307dd35bb515bd7add
kubilaycaglayan <kblycaglayan@gmail.com>
2020-08-03 13:14:48 +0300

    Add mltiple spinner classes into js files

--- src/loading.js
+++ src/loading.js
@@ -1,17 +1,19 @@
 /* eslint-disable no-use-before-define */
 const loading = () => {
-  const spinner = document.getElementsByClassName('spinner-2')[0];
+  const spinners = document.getElementsByClassName('spinner-2');
   const card = document.getElementsByClassName('card')[0];
   const result = document.getElementsByClassName('result')[0];
 
   function show() {
-    spinner.style.display = 'block';
+    spinners[0].style.display = 'block';
+    spinners[1].style.display = 'block';
     card.style.display = 'flex';
     result.style.visibility = 'hidden';
   }
 
   function hide() {
-    spinner.style.display = 'none';
+    spinners[0].style.display = 'none';
+    spinners[1].style.display = 'none';
     card.style.display = 'none';
     result.style.visibility = 'visible';
   }
